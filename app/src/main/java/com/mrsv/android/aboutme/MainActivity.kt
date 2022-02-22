f1300a2d769e3068428f2012cd2252519e2a5cf7
Marcelo Viana <marcelorsviana@gmail.com>
2022-02-22 10:07:55 -0300

    Link the MainActivity with the Data Class MyName and the activity_main.xml layout

--- app/src/main/java/com/mrsv/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/mrsv/android/aboutme/MainActivity.kt
@@ -14,9 +14,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val myName: MyName = MyName("Marcelo R S Viana")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
 
         binding.doneButton.setOnClickListener {
             addNickname(it)
